50482adadb14a4759350be3d886f8baf5cc5d543
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 14:00:59 +0100

    Extended from the abstract integration.

--- src/Integration.php
+++ src/Integration.php
@@ -1,19 +1,11 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_MultiSafepay_Connect_Integration {
+class Pronamic_WP_Pay_Gateways_MultiSafepay_Connect_Integration extends Pronamic_WP_Pay_Gateways_MultiSafepay_AbstractIntegration {
 	public function __construct() {
 		$this->id       = 'multisafepay-connect';
 		$this->name     = 'MultiSafepay - Connect';
 		$this->url      = 'http://www.multisafepay.com/';
 		$this->provider = 'multisafepay';
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_MultiSafepay_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_MultiSafepay_Config';
 	}
 
 	public function get_gateway_class() {
