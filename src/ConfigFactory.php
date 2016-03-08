7cfd992f5c29b097721221c70981a4812cfb272e
Reüel van der Steege <reuel@pronamic.nl>
2016-03-08 09:53:29 +0100

    Fix fatal error 'Call to undefined method Pronamic_WP_Pay_Gateways_MultiSafepay_Config::get_gateway_class()'

--- src/ConfigFactory.php
+++ src/ConfigFactory.php
@@ -12,7 +12,7 @@
  */
 class Pronamic_WP_Pay_Gateways_MultiSafepay_Connect_ConfigFactory extends Pronamic_WP_Pay_GatewayConfigFactory {
 	public function get_config( $post_id ) {
-		$config = new Pronamic_WP_Pay_Gateways_MultiSafepay_Config();
+		$config = new Pronamic_WP_Pay_Gateways_MultiSafepay_Connect_Config();
 
 		$config->mode       = get_post_meta( $post_id, '_pronamic_gateway_mode', true );
 		$config->account_id = get_post_meta( $post_id, '_pronamic_gateway_multisafepay_account_id', true );
